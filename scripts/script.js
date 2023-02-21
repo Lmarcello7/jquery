f2b39186d4e41b337ef1b7d9a4b00bb6b3ce3a44
Lmarcello7 <101017486+Lmarcello7@users.noreply.github.com>
2023-02-20 23:03:30 -0300

    seletores

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-$('div').css('background' , 'lightblue');
+$('div').css({
+    'background' : 'lightblue',
+    'color' : 'red',
+    'font-size' : '20px'
+});
+
+$('td').has('b').css({
+    'background' : 'lightgrey',
+    'color' : 'red'
+});
